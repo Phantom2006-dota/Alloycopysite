a426db0e3fc8d43a95939f1e7ec9b16bc3fc2af0
Phantom2006-dota <danielnworah2006at@gmail.com>
2026-05-17 09:02:06 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -124,7 +124,8 @@ const Header = () => {
           <div className="flex items-center justify-between">
             {/* Logo */}
             <Link to="/" className="flex items-center">
-              <img src={currentLogo} alt={logoAlt} className="h-15 md:h-11 w-auto" />
+              <img src={currentLogo} alt={logoAlt} className="h-15  w-auto" />
+              {/* md:h-11 */}
             </Link>
 
             {/* Desktop Navigation */}
